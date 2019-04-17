cce64a26b5ecdfa4a67bb29ad4e4543319bc0efc
zekrom-vale <sgraven66@gmail.com>
2019-04-17 07:51:03 -0500

    Seperated to into it's own class

---
+++ zAPI/src/zUtil/ArrayTo.java
@@ -0,0 +1,68 @@
+package zUtil;
+
+import java.util.ArrayList;
+
+/**
+ * Converts ArrayLists into arrays
+ *
+ * @author Zekrom
+ *
+ */
+public class ArrayTo{
+	/**
+	 * Converts ArrayList&lt;Double&gt; to double[]
+	 *
+	 * @param  list
+	 *                  The list to convert
+	 * @return      array
+	 */
+	public static double[] Double(final ArrayList<Double> list){
+		return list.stream().mapToDouble(i->i).toArray();
+	}
+
+	/**
+	 * Converts ArrayList<Integer> to int[]
+	 *
+	 * @param  list
+	 *                  The list to convert
+	 * @return      array
+	 */
+	public static int[] Int(final ArrayList<Integer> list){
+		return list.stream().mapToInt(i->i).toArray();
+	}
+
+	/**
+	 * Converts ArrayList<Integer> to int[]
+	 *
+	 * @param  list
+	 *                  The list to convert
+	 * @return      array
+	 */
+	public static int[] Intager(final ArrayList<Integer> list){
+		return list.stream().mapToInt(i->i).toArray();
+	}
+
+
+	/**
+	 * Converts ArrayList<Long> to long[]
+	 *
+	 * @param  list
+	 *                  The list to convert
+	 * @return      array
+	 */
+	public static long[] Long(final ArrayList<Long> list){
+		return list.stream().mapToLong(i->i).toArray();
+	}
+
+
+	/**
+	 * Converts ArrayList<String> to String[]
+	 *
+	 * @param  list
+	 *                  The list to convert
+	 * @return      array
+	 */
+	public static String[] String(final ArrayList<String> list){
+		return list.toArray(new String[0]);
+	}
+}
--- zAPI/src/zUtil/ArrayUtil.java
+++ zAPI/src/zUtil/ArrayUtil.java
@@ -1,6 +1,5 @@
 package zUtil;
 
-import java.util.ArrayList;
 import java.util.function.BinaryOperator;
 import java.util.function.DoubleBinaryOperator;
 import java.util.function.LongBinaryOperator;
@@ -14,72 +13,6 @@ import functions.IndexFunction;
  *
  */
 public final class ArrayUtil{
-	/**
-	 * Converts ArrayLists into arrays
-	 *
-	 * @author Zekrom
-	 *
-	 */
-	public static class to{
-		/**
-		 * Converts ArrayList&lt;Double&gt; to double[]
-		 *
-		 * @param  list
-		 *                  The list to convert
-		 * @return      array
-		 */
-		public static double[] Double(final ArrayList<Double> list){
-			return list.stream().mapToDouble(i->i).toArray();
-		}
-
-		/**
-		 * Converts ArrayList<Integer> to int[]
-		 *
-		 * @param  list
-		 *                  The list to convert
-		 * @return      array
-		 */
-		public static int[] Int(final ArrayList<Integer> list){
-			return list.stream().mapToInt(i->i).toArray();
-		}
-
-		/**
-		 * Converts ArrayList<Integer> to int[]
-		 *
-		 * @param  list
-		 *                  The list to convert
-		 * @return      array
-		 */
-		public static int[] Intager(final ArrayList<Integer> list){
-			return list.stream().mapToInt(i->i).toArray();
-		}
-
-
-		/**
-		 * Converts ArrayList<Long> to long[]
-		 *
-		 * @param  list
-		 *                  The list to convert
-		 * @return      array
-		 */
-		public static long[] Long(final ArrayList<Long> list){
-			return list.stream().mapToLong(i->i).toArray();
-		}
-
-
-		/**
-		 * Converts ArrayList<String> to String[]
-		 *
-		 * @param  list
-		 *                  The list to convert
-		 * @return      array
-		 */
-		public static String[] String(final ArrayList<String> list){
-			return list.toArray(new String[0]);
-		}
-	}
-
-
 	/**
 	 * Returns the max value of an array
 	 *
